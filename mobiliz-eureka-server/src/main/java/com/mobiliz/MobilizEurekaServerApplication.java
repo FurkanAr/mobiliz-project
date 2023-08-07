5e609a4f4709beee97ada2e7101ce4654b54d12f
Furkan Ar <far.farnt@gmail.com>
2023-08-07 23:33:56 +0300

    eureka-server created

---
+++ mobiliz-eureka-server/src/main/java/com/mobiliz/MobilizEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.mobiliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MobilizEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilizEurekaServerApplication.class, args);
+	}
+
+}
